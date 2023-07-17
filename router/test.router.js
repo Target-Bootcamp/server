c3f16cc649cb06f71ea50600441deea09adeeae5
sagi buta <sagibuta@gmail.com>
2023-07-18 00:52:33 +0300

    edit router & checked

--- router/test.router.js
+++ router/test.router.js
@@ -2,7 +2,6 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/', (req, res) => {
-
     try {
         res.send(" root shomeaa port 3008 ")
         console.log("ok");
