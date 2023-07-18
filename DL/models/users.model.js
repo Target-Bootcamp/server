2113afa7dcfcf1bade391c2be9ae0b81b5358184
AviadDerli <aviad@workin.co.il>
2023-07-19 00:49:08 +0300

    fix schema actions

--- DL/models/users.model.js
+++ DL/models/users.model.js
@@ -2,16 +2,21 @@ const mongoose = require('mongoose');
 
 
 const usersSchema = new mongoose.Schema(
-    {
-      userId: {type:Number, required:true},
-      fName: {type:String, required:true},
-      lName: {type:String, required:true,},
-      phone: {type:Number, required:true},
-      email: {type:String, required:true},
-      participantNum:{type:Number, required:true},
-      comments: {type:String},
-      permission: {type:String, required:true}
+  {
+    userId: { type: Number },
+    fName: { type: String },
+    lName: { type: String, },
+    phone: { type: Number },
+    email: { type: String },
+    participantNum: { type: Number },
+    comments: { type: String },
+    permission: {
+      type: String,
+      enum: ["admin", "student", "lecturer"],
+      default: "student"
+
     }
+  }
 )
 
 const usersModel = mongoose.model("users", usersSchema)
