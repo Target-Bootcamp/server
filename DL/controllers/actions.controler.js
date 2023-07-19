0e38e1164d32d13274cb0d204d093181867c044c
avi1998s <avi6190i@gmail.com>
2023-07-19 11:26:01 +0300

    .

--- DL/controllers/actions.controler.js
+++ DL/controllers/actions.controler.js
@@ -1,10 +1,22 @@
 const actionsModel = require('../models/actions.model');
 
 
-const read = async (filterBy) => await actionsModel.findOne(filterBy)
-const create = async (newData) => await actionsModel.create(newData)
-const del = async (filterBy) => await actionsModel.findByIdAndUpdate(filterBy, { IS: "? no active" })
-const update = async (infoUpdate) => await actionsModel.findByIdAndUpdate({ _id: infoUpdate._id }, infoUpdate.updateaction)
+const read = async (filterBy) =>{
+    let data = await actionsModel.findOne(filterBy)
+    return data
+}
+const create = async (newData) => {
+    let data = await actionsModel.create(newData)
+    return data
+}
+const del = async (filterBy) => {
+    let data = await actionsModel.findByIdAndUpdate(filterBy, { IS: "? no active" })
+    return data
+}
+const update = async (infoUpdate) => {
+    let data = await actionsModel.findByIdAndUpdate({ _id: infoUpdate._id }, infoUpdate.updateaction)
+    return data
+}
 
 
 module.exports = {read, create,  del, update }
