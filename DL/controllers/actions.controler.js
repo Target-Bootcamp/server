1799d08c0e96af0e630bb23d45759785c45ada03
sagi buta <sagibuta@gmail.com>
2023-07-20 15:49:29 +0300

    fix crud actions

--- DL/controllers/actions.controler.js
+++ DL/controllers/actions.controler.js
@@ -1,34 +1,42 @@
 const actionsModel = require('../models/actions.model');
-const {} = require('../../functions/fs.functions')
-const {uploadFile} = require('../../functions/upload.functions')
+const { } = require('../../functions/fs.functions')
+const { uploadFile } = require('../../functions/upload.functions')
 
 const read = async (filterBy) => {
     let data = await actionsModel.find(filterBy)
     return data
 }
-
-
-// readDates().then(console.log)
+const readOne = async (filterBy) => {
+    let data = await actionsModel.findOne({ _id: filterBy })
+    return data
+}
+// readOne("64b7a473e400aa13afad6e78").then(console.log)
 const create = async (newData) => {
     let data = await actionsModel.create(newData)
     return data
+}
+const update = async (id, newData) => {
+    return await actionsModel.findByIdAndUpdate(id, newData, { new: true })
 }
 const deleteOne = async (id) => {
     let data = await actionsModel.findByIdAndRemove(id)
     return data
 }
-const getNested=async(actionId,arrKey)=>{
-
+const getNested = async (actionId, arrKey) => {
+    let data = await actionsModel.findOne({ _id: actionId })
+    return data[arrKey]
 }
+// getNested("64b7a473e400aa13afad6e78", "tasks").then(console.log)
 async function updateNested(actionId, arrName, objectId, dataToUpdateKey, dataToUpdateVal) {
-    await actionsModel.findOneAndUpdate(
+    let data = await actionsModel.findOneAndUpdate(
         { _id: actionId, [`${arrName}._id`]: objectId },
         { $set: { [`${arrName}.$.${dataToUpdateKey}`]: dataToUpdateVal } },
         { new: true }
     )
-
-    return read({_id:actionId})
+    // return data
+    return (getNested(actionId, arrName))
 }
+//  updateNested("64b6bd3491215e213dfe2bad","schedules","64b6bd3491215e213dfe2bb3","comments","fffff").then(console.log();)
 const readDates = async (date1, date2, arrKey, key) => {
     // date1 = new Date()
     // date2 = new Date(date1.getTime() + 7 * 24 * 60 * 60 * 1000)
@@ -53,13 +61,20 @@ const readDates = async (date1, date2, arrKey, key) => {
     })
     return newdata
 }
-const update = async (id, newData) => {
-    return await actionsModel.findByIdAndUpdate(id, newData, { new: true })
-}
+
+
+
+
+
+
+
+
+// readDates().then(console.log)
+
 
 // TODO :
 //  - readOne (findOne)
 //  - readNested 
 
 
-module.exports = { read, create, update, updateNested, deleteOne, readDates,getNested }
+module.exports = { read, create, update, updateNested, deleteOne, readDates, getNested, readOne }
--- router/actions.router.js
+++ router/actions.router.js
@@ -3,31 +3,31 @@ const router = express.Router()
 const { createFun, readFun, deleteFun, updateFun, updateNestedFun, getDatesFun, getNestedFun } = require('../BL/services/actions.services')
 const { getMonthRange } = require('../functions/monthdate')
 // fs
-const {checkIfEmpty,renameFile,crateFolder,crateFile,editFile,readFile,readFolder,deleteFF,claerFolder} = require("../functions/fs.functions")
+const { checkIfEmpty, renameFile, crateFolder, crateFile, editFile, readFile, readFolder, deleteFF, claerFolder } = require("../functions/fs.functions")
 
 const { uploadFile } = require("../functions/upload.functions")
-const {fuulDateOver,getDate,getOuer,getOuerMS} = require('../functions/getTime.functions')
+const { fuulDateOver, getDate, getOuer, getOuerMS } = require('../functions/getTime.functions')
 
 const root = "./DL/root"
 
 // bnana
-// claerFolder(`${root}/bnana`)
+claerFolder(`${root}/bnana`)
 
-router.post('/:folder',uploadFile("file"), async (req, res) => {
+router.post('/:folder', uploadFile("file"), async (req, res) => {
     const file = req.file
     const folder = req.params.folder
     const fileName = file.originalname
-    const folderPath =`${root}/${folder}`
+    const folderPath = `${root}/${folder}`
     try {
         crateFolder(folderPath)
-        renameFile(file.path,`${folderPath}/${fuulDateOver}__${fileName}`)
+        renameFile(file.path, `${folderPath}/${fuulDateOver}__${fileName}`)
         const data = {
             fileType: file.mimetype.split("/")[0],
             size: file.size,
             fileName,
             createdDate: getDate(),
             createdOuer: getOuer(),
-            filePath:`${folderPath}/${fuulDateOver}__${fileName}`
+            filePath: `${folderPath}/${fuulDateOver}__${fileName}`
         }
         console.log(data);
         res.send(file)
@@ -37,8 +37,8 @@ router.post('/:folder',uploadFile("file"), async (req, res) => {
     }
 })
 
- router.get('/:actionId/:arrKey', async (req, res) => {
-    const { actionId, arrKey} = req.params
+router.get('/:actionId/:arrKey', async (req, res) => {
+    const { actionId, arrKey } = req.params
     try {
         const result = await getNestedFun(actionId, arrKey,)
         res.send(result)
@@ -46,9 +46,9 @@ router.post('/:folder',uploadFile("file"), async (req, res) => {
         console.log(error);
         res.status(400).send(error)
     }
- })
+})
 
- // router.get('/:actionId/:arrKey/:kId', async (req, res) => {})
+// router.get('/:actionId/:arrKey/:kId', async (req, res) => {})
 
 // router.post('/:actionId/:arrKey', async (req, res) => {})
 
@@ -59,7 +59,7 @@ router.put('/:actionId/:arrKey/:kId', async (req, res) => {
         res.send(result)
     } catch (error) {
         console.log(error);
-        res.status(400).send(error)
+        res.status(400).send(error.message || error)
     }
 })
 
@@ -108,7 +108,7 @@ router.get('/:selctor/:key', async (req, res) => {
         res.status(400).send(error.message)
     }
 })
-// router.get('/actionId/:selctor/:month', async (req, res) => {
+router.get('/actionId/:selctor/:month', async (req, res) => {
 
     const { start, end } = getMonthRange(year, month)
     const { selctor, key } = req.params
@@ -118,7 +118,7 @@ router.get('/:selctor/:key', async (req, res) => {
     } catch (error) {
         res.status(400).send(error.message)
     }
-// })
+})
 router.delete("/:id", async (req, res) => {
     try {
         let data = await deleteFun(req.params.id)
